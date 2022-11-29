66a0cf4a68dcd431e4231c44aedd0edc83fad283
Garnik Hovsepyan <garoh2291@gmail.com>
2022-11-29 10:49:54 +0400

    change privacy

--- src/data/index.js
+++ src/data/index.js
@@ -20,4 +20,3 @@ export const NAVBAR_PAGES = {
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const IMG_URL = "https://lsm-static-prod.livescore.com/medium";
-const test = 1;
